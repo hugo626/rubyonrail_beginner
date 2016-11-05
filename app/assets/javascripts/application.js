5c638b5e4d937cefa8ce330aa3d897451ce03b28
Yuguo LI <hugo626@gmail.com>
2016-11-05 12:32:32 +0000

    add new theme to this job offer website

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 // = require jquery
 // = require bootstrap-sprockets
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+// = require jquery_ujs
+// = require turbolinks
+// = require_tree ./sitewide
